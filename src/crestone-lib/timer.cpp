4f11573e6184a8bcb6d97d3bf58dd0d60ab36f41
Tech0tron <tech0tron.roblox@gmail.com>
2022-04-26 21:32:58 -0600

    fix timer

--- src/crestone-lib/timer.cpp
+++ src/crestone-lib/timer.cpp
@@ -1,9 +1,11 @@
 #include "timer.hpp"
+#include <iostream>
 
 void Timer::execute() {
     // from emulator::execute()
     dt += std::chrono::milliseconds(2).count();
-    if (dt > 16667) {
+    std::cout << dt << std::endl;
+    if (dt > 16) {
         dt = 0;
         if (emulator_state->sound_timer > 0) {
             emulator_state->sound_timer--;
